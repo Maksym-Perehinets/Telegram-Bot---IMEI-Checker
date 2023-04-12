87b033bee5b8e661b4f2ce7eecb0c2d080ce2823
max <supercomf128@gmail.com>
2023-04-12 23:33:37 +0300

    1

--- bot/bot.py
+++ bot/bot.py
@@ -7,32 +7,37 @@ from config import BOT_TOKEN
 bot = TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start'])  # Start comad hendler
 def start_command(msg: types.Message):
     bot.send_message(msg.chat.id, "Привіт тепер ти можеш мене використовувати!")
+    # Redirecting to the next stage
     bot.register_next_step_handler(msg, buttons)
 
 
-@bot.message_handler(commands=['button'])
+@bot.message_handler(commands=['button'])  # Defining a function with buttons
 def buttons(msg: types.Message):
+    # Creation of buttons
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    markup.add(KeyboardButton('Cheak my imei'), KeyboardButton('Test http req'), KeyboardButton('None'))
+    # Creation of main butttons
+    markup.add(
+        KeyboardButton('Check my imei'),
+        KeyboardButton('Test http req'),
+        KeyboardButton('None'))
     bot.send_message(msg.chat.id, 'chose what you need', reply_markup=markup)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text'])  # Button press hendler
 def button_answ(msg: types.Message):
-    if msg.text == 'Cheak my imei':
+    if msg.text == 'Check my imei':  # Case one(checking imei actuality)
         bot.send_message(msg.chat.id, 'Enter your imei:')
         bot.register_next_step_handler(msg, imei_cheak)
-    elif msg.text == 'Test http req':
+    elif msg.text == 'Test http req':  # Case two(sending a test request)
         bot.send_message(msg.chat.id, str(ImeiRequests.test_request()))
         bot.register_next_step_handler(msg, imei_cheak)
-    bot.register_next_step_handler(msg, buttons)
+    bot.register_next_step_handler(msg, buttons)  # Returns to options
 
 
-
-def imei_cheak(msg: types.Message):
+def imei_cheak(msg: types.Message):  # Doing shit and lie =)
     bot.send_message(msg.chat.id, f"imei {msg.text} is valid")
 
 
--- bot/imei_http_req.py
+++ bot/imei_http_req.py
@@ -3,8 +3,23 @@ from config import API_KEY
 test = f'http://api-client.imei.org/api/services?apikey={API_KEY}'
 
 
+prices = []
+
+
 class ImeiRequests:
+
+    def get_price():
+        # getting a json with prices and other data
+        req = requests.get(test).json()
+        # creating a list with item id`s name and prices
+        for item in req["response"]["services"]:
+            prices.append(item)
+
     def test_request():
+        # getting a json with prices and other data
         result = requests.get(test).json()
         print(result)
         return result
+
+
+ImeiRequests.get_price()
