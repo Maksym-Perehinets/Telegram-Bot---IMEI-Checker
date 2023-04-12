6afc2956c7b3301e1ac8ae879b21ab1b8d60283f
max <supercomf128@gmail.com>
2023-04-12 14:55:00 +0300

    1

--- bot/bot.py
+++ bot/bot.py
@@ -1,50 +1,37 @@
 from telebot import TeleBot, types
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+import json
 from config import BOT_TOKEN
 
-bot = TeleBot(BOT_TOKEN)
-r = 2
 
-huylo = 5
-users = {} 
+bot = TeleBot(BOT_TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start_command(msg: types.Message):
-    bot.send_message(msg.chat.id, 'Enter your name:')
-    bot.register_next_step_handler(msg, get_user_name)
+    bot.send_message(msg.chat.id, "Привіт тепер ти можеш мене використовувати!")
+    bot.register_next_step_handler(msg, buttons)
 
-def get_user_name(msg: types.Message):
-    name = msg.text
-    users[msg.from_user.id] = {'name': msg.text}
-    bot.send_message(msg.chat.id, f'Hi {name}, how old  are you ')
-    bot.register_next_step_handler(msg, get_user_age)
 
-def get_user_age(msg: types.Message):
-    age = msg.text
-    try:
-        age = int(age)
-    except ValueError:
-        bot.send_message(msg.chat.id, 'Enter a number')
-        bot.register_next_step_handler(msg, get_user_age)
-        return
+@bot.message_handler(commands=['button'])
+def buttons(msg: types.Message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.add(KeyboardButton('Cheak my imei'), KeyboardButton('Say my name'), KeyboardButton('None'))
+    bot.send_message(msg.chat.id, 'chose what you need', reply_markup=markup)
 
-    user = users[msg.from_user.id]
-    user['age'] = age
 
-    bot.send_message(msg.chat.id, f'great! {user["name"]} Now enter your gender (M or F)')
-    bot.register_next_step_handler(msg, get_user_gender)
+@bot.message_handler(content_types=['text'])
+def button_answ(msg: types.Message):
+    if msg.text == 'Cheak my imei':
+        bot.send_message(msg.chat.id, 'Enter your imei:')
+    elif msg.text == 'Say my name':
+        msg_json = msg.json['from']
+        bot.send_message(msg.chat.id, str(msg_json['first_name']))
+    bot.register_next_step_handler(msg, imei_cheak)
 
-def get_user_gender(msg: types.Message):
-    user = users[msg.from_user.id]
-    gender = msg.text
-    if gender not in ('M', 'F'):
-        bot.send_message(msg.chat.id, 'something went wrong ')
-        bot.register_next_step_handler(msg, get_user_gender)
-        return
-    user = users[msg.from_user.id]
-    user['gender'] = gender
 
-    bot.send_message(msg.chat.id, f'Uhu your name:{user["name"]}\n your age {user["age"]}\n your gender {user["gender"]}',
-    )
+def imei_cheak(msg: types.Message):
+    bot.send_message(msg.chat.id, f"imei {msg.text} is valid")
+
 
 bot.infinity_polling()
