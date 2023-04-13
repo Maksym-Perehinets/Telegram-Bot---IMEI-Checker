047646548b5c4429ed36e09534b9617378c33ead
max <supercomf128@gmail.com>
2023-04-13 12:48:36 +0300

    test

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,8 @@
 from telebot import TeleBot, types
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from imei_http_req import ImeiRequests
 from config import BOT_TOKEN
+from time import sleep
+from  ReplyKeyboard import markup
 
 
 bot = TeleBot(BOT_TOKEN)
@@ -16,14 +17,7 @@ def start_command(msg: types.Message):
 
 @bot.message_handler(commands=['button'])  # Defining a function with buttons
 def buttons(msg: types.Message):
-    # Creation of buttons
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    # Creation of main butttons
-    markup.add(
-        KeyboardButton('Check my imei'),
-        KeyboardButton('Test http req'),
-        KeyboardButton('None'))
-    bot.send_message(msg.chat.id, 'chose what you need', reply_markup=markup)
+    bot.send_message(msg.chat.id, 'Menue', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])  # Button press hendler
@@ -33,12 +27,17 @@ def button_answ(msg: types.Message):
         bot.register_next_step_handler(msg, imei_cheak)
     elif msg.text == 'Test http req':  # Case two(sending a test request)
         bot.send_message(msg.chat.id, str(ImeiRequests.test_request()))
-        bot.register_next_step_handler(msg, imei_cheak)
+        bot.register_next_step_handler(msg, buttons)
     bot.register_next_step_handler(msg, buttons)  # Returns to options
 
 
 def imei_cheak(msg: types.Message):  # Doing shit and lie =)
     bot.send_message(msg.chat.id, f"imei {msg.text} is valid")
+    bot.register_next_step_handler(msg, buttons)
 
-
-bot.infinity_polling()
+if __name__ == '__main__':
+        try:
+            bot.infinity_polling()
+        except:
+            sleep(10)
+            bot.infinity_polling()
