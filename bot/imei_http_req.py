afc2b8a6b3e776acc7aba9d4fa669d7a4e5c00e2
max <supercomf128@gmail.com>
2023-04-13 22:20:02 +0300

    test

--- bot/imei_http_req.py
+++ bot/imei_http_req.py
@@ -1,61 +1,38 @@
 import requests
 from config import API_KEY
-test = f'http://api-client.imei.org/api/services?apikey={API_KEY}'
+api_requests = f'http://api-client.imei.org/api/services?apikey={API_KEY}'
 
-services_id = 0
-
+services = {}
 prices = []
 
 
 class ImeiRequests:
 
-    def output(req): # to tak nepracuie
+    def output(req):  # to tak nepracuie
         for item in req["response"]["services"]:
             prices.append(item)
         for item in prices:
-            print(item)
+            return item
 
-    def apple_advanced_check(imei):
-        services_id = 50
-        req = requests.get(f"http://api-client.imei.org/api/submit?apikey={API_KEY}&service_id={services_id}&input={imei}").json
-        return
 
-    def apple_basic_check(imei):
-        services_id = 3
-        return
+    #Geting
+    def geting_valid_price_information():
+        response = requests.get(api_requests).json()
+        for item in response['response']['services']:
+            services
 
-    def apple_carrier_check(imei):
-        services_id = 17
-        return
 
-    def apple_fms_check(imei):
-        services_id = 2
-        return
 
-    def apple_mdm_check(imei):
-        services_id = 28
-        return
 
-    def apple_mdmpro_check(imei):
-        services_id = 155
-        return
 
-    def apple_warranty_status_check(imei):
-        services_id = 4
-        return
-
-    def apple_simlock_check(imei):
-        services_id = 16
-        return
-
-    def get_price():
-        # getting a json with prices and other data
-        req = requests.get(test).json()
-        # creating a list with item id`s name and prices
-        for item in req["response"]["services"]:
-            prices.append(item)
-        for item in prices:
-            print(item)
+    # def get_price():
+    #    # getting a json with prices and other data
+    #     req = requests.get(test).json()
+    #     # creating a list with item id`s name and prices
+    #     for item in req["response"]["services"]:
+    #         prices.append(item)
+    #     for item in prices:
+    #         print(item)
 
     def test_request():
         # getting a json with prices and other data
@@ -63,5 +40,4 @@ class ImeiRequests:
         print(result)
         return result
 
-
-ImeiRequests.get_price()
+ImeiRequests.geting_valid_price_information()
