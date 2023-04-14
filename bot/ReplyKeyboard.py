cdd153ba1a6bebd39b220ec6b0d46b75fc55f477
max <supercomf128@gmail.com>
2023-04-14 14:35:09 +0300

    test branch

--- bot/ReplyKeyboard.py
+++ bot/ReplyKeyboard.py
@@ -4,6 +4,18 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 # Creation of main butttons
 markup.add(
-    KeyboardButton('Test api request'),
-    KeyboardButton(''),
-    KeyboardButton('None'))
+    KeyboardButton('Icloud ON/Of'),
+    KeyboardButton('Icloud clean/lost'),
+    KeyboardButton('Перевірка оператора'),
+    KeyboardButton('Перевірка lock/unlock'),
+    KeyboardButton('Перевірка MDM статусу Iphone'),
+    KeyboardButton('Перевірка Icloud на Mac'),
+    KeyboardButton('Serial number to IMEI'),
+    KeyboardButton('IMEI to serial number'),
+    KeyboardButton('Iphone Basic info'),
+    KeyboardButton('Activation check'),
+    KeyboardButton('Дата Купівлі'),
+    KeyboardButton('Перевірка MDM (Mac, Ipad, Iphone)'),
+    KeyboardButton('Мій аккаунт'),
+    KeyboardButton('Перевірка оператора')
+    )
