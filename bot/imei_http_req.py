26917918ede00ddb19f82f7638c16d57fbb483da
max <supercomf128@gmail.com>
2023-04-15 22:12:01 +0300

    test branch

--- bot/imei_http_req.py
+++ bot/imei_http_req.py
@@ -7,30 +7,26 @@ prices = []
 
 
 class InvalidImeiServerResponse(Exception):    # Server response error exception
-    """Raised when http request return status==0"""
-    pass
-
-
-class NotEnoughMoneyInBalance(Exception):    # Balance error exception
-    """Raised when balance on lower then 1$"""
+    """Raised when http request return status==0 or not enough money in balance"""
     pass
 
 
 class ImeiRequests:
 
     def output(self):    # Function that outputs data from http response
-        pass
+        return self
 
-    # Geting valid pricec and services id`s
-    def valid_price_and_balance_check(self):
+    # Getting valid prices and services id`s
+    def valid_price_and_balance_check():
         response = requests.get(api_requests).json()
+        balance_check = requests.get(f'https://api-client.imei.org/api/balance?apikey={API_KEY}').json()
         # checks if server is reachable and input data correct else
-        if response['status'] != 1:
+        if response['status'] != 1 or balance_check['response']['credits'] > 1:
             raise InvalidImeiServerResponse
         for item in response['response']['services']:    # Loop which converts api answer json file to
             # Adding new item with value of key==to name in json and array [a<--Id, b<--price]
             services.update({item.get('name'): [item.get('id'), item.get('price')]})
-        return services
+        return ImeiRequests.output(services)
 
     def test_request(self):
         # getting a json with prices and other data
