f90b638fc6a7b255ef4f30ab3fe799bccb5a2b98
Vadym <vadimkurdidiK@gmail.com>
2023-04-27 15:37:32 +0300

    1

---
+++ bot/SqlConnector.py
@@ -0,0 +1,10 @@
+import pyodbc
+import mysql.connector
+import bot
+mydb = mysql.connector.connect(
+      host="DESKTOP-PNKODS9",
+      user="root",
+      password="#Hk54KG8gkPt",
+      database="test_db"
+)
+user_data = []
