ad164fa093b3705640064cd2a8f7235a2a125cb9
Maksym-Perehinets <90769932+maksym-perehinets@users.noreply.github.com>
2023-06-18 17:38:45 +0300

    Update README.md

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ from telebot import TeleBot, types
 from imei_http_req import ImeiRequests
 from config import BOT_TOKEN
 from time import sleep
+from Inline_keyboard import Inline_keyboard_one, Inline_keyboard_two
 from ReplyKeyboard import markup, markup2
 
 bot = TeleBot(BOT_TOKEN)
@@ -9,30 +10,20 @@ bot = TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])  # Start comad hendler
 def start_command(msg: types.Message):
-    bot.send_message(msg.chat.id, "Привіт я розроблений з метою полегшення перевірки IMEI твого iPhone")
     # Redirecting to the next stage
-    bot.register_next_step_handler(msg, buttons)
+    bot.send_message(msg.chat.id, "Привіт я розроблений з метою полегшення перевірки IMEI твого iPhone. "
+                                  "Коротнка інформація. Для поповнення балансу напишіть @user", reply_markup=Inline_keyboard_one)
 
 
-@bot.message_handler(commands=['button'])  # Defining a function with buttons
-def buttons(msg: types.Message):
-    bot.send_message(msg.chat.id, 'Доступні опції', reply_markup=markup)
-    bot.register_next_step_handler(msg, button_answ)
-
-
-# Button press handler
-def button_answ(msg: types.Message):
-    if msg.text == 'Check my imei':  # Case one(checking imei actuality)
-        bot.send_message(msg.chat.id, 'Enter your imei:')
-    elif msg.text == 'Icloud ON/Of':  # Case two(sending a test request)
-        bot.send_message(msg.chat.id, str(ImeiRequests.valid_price_and_balance_check()))
-        bot.send_message(msg.chat.id, 'Бажаєте вернутись назад', reply_markup=markup2)
-        bot.register_next_step_handler(msg, buttons)  # Returns to menu
-    else:
-        bot.send_message(msg.chat.id, 'Incorrect command')
-        bot.register_next_step_handler(msg, buttons)
-
-
+@bot.callback_query_handler(func=lambda msg: True)
+def response(msg):
+    if msg.data == "Main_Func":
+        ImeiRequests.geting_valid_price_information()
+        bot.send_message(msg.message.chat.id, "Menue", reply_markup=markup)
+@bot.message_handler(content_types=["text"])
+def buttons(msg):
+    if msg.text == "Icloud ON/Of":
+        bot.send_message(msg.chat.id, )
 if __name__ == '__main__':
     try:
         bot.infinity_polling()
