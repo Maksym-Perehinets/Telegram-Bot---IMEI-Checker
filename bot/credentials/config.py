0943c6826255a79fb2b427ca53e2c20aba36f4df
Maksym-Perehinets <90769932+Maksym-Perehinets@users.noreply.github.com>
2024-01-28 20:23:13 +0200

    Update config.py

--- bot/credentials/config.py
+++ bot/credentials/config.py
@@ -3,11 +3,11 @@ BOT_TOKEN = '6290787018:AAHbQvjCEzj4_vMOwCb_BXmKpLphCSw1EdE'
 BOT_OWNER = ' '
 
 # IMEI API KEYS and LINKS
-API_KEY = 'OjjlfRQBVoMdJMs9bO8QUGM6YKkWuVKIi91WdssIEhHCMQF8aMeM0g0VtaNa'
+API_KEY = ''
 api_requests_service_list = f'http://api-client.imei.org/api/services?apikey={API_KEY}'
 api_requests_order = f'http://api-client.imei.org/api/submit?apikey={API_KEY}'
 
 # GOOGLE sheet API imports
 CRED_FILE = "bot/credentials/cred.json"
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-SHEET_ID = "1QYKrweZnNv9cIU2msMZ34yCf1famnV0JzG4bitpZxI0"
+SHEET_ID = ""
